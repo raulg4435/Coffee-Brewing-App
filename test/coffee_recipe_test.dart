5db7034925480fde9b2e3474bdc5c159f6b036d8
Eric Martinez <eric.michael.mtz@gmail.com>
2020-11-29 15:44:10 -0600

    empty whitespace

--- test/coffee_recipe_test.dart
+++ test/coffee_recipe_test.dart
@@ -6,7 +6,7 @@ void main() {
   test('creates a valid coffee recipe', () {
     // make a coffee recipe
 
-    //check that it has the right data
+    // check that it has the right data
   });
 
   //how do we test rejecting invalid coffee recipes?
